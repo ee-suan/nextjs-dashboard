f6a76bb78f17d2b66108cd3d11d4a973a658aae6
eesuan <eesuan.234@gmail.com>
2024-02-04 21:44:22 +0800

    Update app name

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -7,6 +7,12 @@ const users = [
     email: 'user@nextmail.com',
     password: '123456',
   },
+  {
+    id: '410244f2-4001-4271-9855-fec4b6a6442a',
+    name: 'User2',
+    email: 'user2@nextmail.com',
+    password: '123456',
+  }
 ];
 
 const customers = [
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,13 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { ClipboardDocumentCheckIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    <div 
+      className={`${lusitana.className} flex flex-row items-center justify-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <ClipboardDocumentCheckIcon className="h-12 w-12 rotate-[5deg]" />
+      <p className="text-[44px]">Traqq</p>
     </div>
   );
 }
